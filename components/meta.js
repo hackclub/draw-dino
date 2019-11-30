b4c68513e34a4593b9dc4ea7ef8182c946af8fc1
Max Wofford <max@maxwofford.com>
2019-11-30 14:56:44 -0500

    Switch to Phantom Sans

--- components/meta.js
+++ components/meta.js
@@ -1,5 +1,15 @@
 import Head from 'next/head'
 
+const fontImport = (family, weight, file) => (`
+@font-face {
+  font-family: '${family}';
+  font-weight: ${weight};
+  src: url('fonts/${file}.woff2') format('woff2'),
+       url('fonts/${file}.woff') format('woff'),
+       url('fonts/${file}.ttf') format('truetype');
+}
+`)
+
 export default () => (
   <>
     <Head>
@@ -7,12 +17,32 @@ export default () => (
       <meta charSet="utf-8" />
     </Head>
     <style jsx global>{`
+      @import url('https://fonts.googleapis.com/css?family=Roboto&display=swap');
+      ${fontImport('Phantom Sans', '400', 'PhantomSans0.6-Regular')}
+      ${fontImport('Phantom Sans', '500', 'PhantomSans0.6-Book')}
+      ${fontImport('Phantom Sans', '600', 'PhantomSans0.6-Medium')}
+      ${fontImport('Phantom Sans', '700', 'PhantomSans0.6-Semibold')}
+      ${fontImport('Phantom Sans', '800', 'PhantomSans0.6-Bold')}
+      ${fontImport('Phantom Sans', 'italic', 'PhantomSans0.6-Italic')}
+
+
       body {
         margin: 0;
-        font-family: 'Roboto', sans-serif;
+        font-family: 'Phantom Sans', 'Roboto', sans-serif;
       }
 
-      @import url('https://fonts.googleapis.com/css?family=Roboto&display=swap');
+
+      h1, h2, h3 {
+        font-family: 'Phantom Sans';
+        font-weight: 800;
+      }
+
+      a {
+        color: inherit;
+        font-weight: bold;
+      }
+
+
     `}</style>
   </>
 )
