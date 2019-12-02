bc2ccb0a67af6e064d95ae2d5436a852e1479183
Max Wofford <max@maxwofford.com>
2019-12-02 16:47:41 -0500

    Steps are revealed by the user

--- components/step.js
+++ components/step.js
@@ -1,3 +1,5 @@
+import Scroll from 'react-scroll'
+
 const containerStyle = {
   width: '100%',
   height: '100vh',
@@ -6,7 +8,7 @@ const containerStyle = {
   overflow: 'hidden',
   background: 'white',
   display: 'grid',
-  gridTemplateColumns: '50% 50%'
+  gridTemplateColumns: '50% 50%',
 }
 
 const sideStyle = {
@@ -16,41 +18,69 @@ const sideStyle = {
   justifyContent: 'center',
   textAlign: 'center',
   color: 'white',
-  background: '#333'
+  background: '#333',
 }
 
 const rightStyle = {
   padding: '3em',
   background: '#eee',
   background: 'linear-gradient(#eee, #eaeaea)',
-  color: '#222'
+  color: '#222',
 }
 
 const subStyle = {
   fontStyle: 'italic',
-  opacity: 0.7
+  opacity: 0.7,
 }
 
 const imageStyle = {
   maxWidth: '100%',
   borderRadius: '0.5em',
-  boxShadow: 'rgba(0, 0, 0, 1) 0 0 1em, rgba(255, 255, 255, 0.1) 0 0 5em'
+  boxShadow: 'rgba(0, 0, 0, 1) 0 0 1em, rgba(255, 255, 255, 0.1) 0 0 5em',
 }
 
-const Step = ({ subtitle, image, imageLink, children }) => (
-  <div style={containerStyle}>
-    <div style={sideStyle}>
-      {image && (
-        <>
-          <a target="_blank" href={imageLink}>
-            <img src={image} style={imageStyle} />
-          </a>
-          <p style={subStyle}>{subtitle || image}</p>
-        </>
-      )}
-    </div>
-    <div style={{ ...sideStyle, ...rightStyle }}>{children}</div>
-  </div>
-)
+const hiddenStyle = {
+  display: 'none',
+}
 
-export default Step
+export default ({
+  index,
+  progress,
+  setProgress,
+  subtitle,
+  image,
+  imageLink,
+  children,
+}) => {
+  const isHidden = index > progress
+
+  if (isHidden) {
+    return null
+  }
+
+  const handleClick = () => {
+    if (progress < index + 1) {
+      setProgress(index + 1)
+    }
+    Scroll.animateScroll.scrollToBottom()
+  }
+
+  return (
+    <div style={isHidden ? hiddenStyle : containerStyle} index={index}>
+      <div style={sideStyle}>
+        {image && (
+          <>
+            <a target="_blank" href={imageLink}>
+              <img src={image} style={imageStyle} />
+            </a>
+            <p style={subStyle}>{subtitle || image}</p>
+          </>
+        )}
+      </div>
+      <div style={{ ...sideStyle, ...rightStyle }}>
+        {children}
+        <button onClick={handleClick}>Next step</button>
+      </div>
+    </div>
+  )
+}
