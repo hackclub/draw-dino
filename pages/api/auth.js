290fc50a45ffd09c30457cf43548cebc0ba72997
JosiasAurel <ndjosiasaurel@gmail.com>
2024-02-15 15:23:36 +0100

    upgrade next version and add auth function in next.js api directory

---
+++ pages/api/auth.js
@@ -0,0 +1,40 @@
+const {
+  CLIENT_ID, CLIENT_SECRET
+} = process.env
+const SlackStrategy = require('passport-slack').Strategy
+const passport = require('passport')
+const express = require('express')
+const fetch = require('node-fetch')
+const app = express()
+
+passport.use(new SlackStrategy({
+  clientID: CLIENT_ID,
+  clientSecret: CLIENT_SECRET,
+  state: true
+}, (accessToken, refreshToken, profile, done) => {
+  done(null, profile)
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(require('body-parser').urlencoded({ extended: true }))
+
+app.get(
+  '/update-github-url',
+  passport.authorize('slack'),
+  (req, res) => {
+    const data = {
+      slack: req.account.user.id,
+      github: req.query.state
+    }
+
+    const webhook = 'https://hooks.zapier.com/hooks/catch/507705/odyc4wo/'
+    fetch(webhook, {
+      method: 'POST',
+      body: JSON.stringify(data)
+    })
+    return res.redirect(302, "https://draw-dino.hackclub.com/slackAuthSuccess")
+  }
+)
+
+module.exports = app
