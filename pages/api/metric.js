06d5ed99ad781fff4010e02af5b2ccc59a087d8f
JosiasAurel <ndjosiasaurel@gmail.com>
2024-03-11 12:31:02 +0100

    fix api endpoint

--- pages/api/metric.js
+++ pages/api/metric.js
@@ -1,12 +1,15 @@
 import metrics from "../../metrics";
 
 export default async function sendMetric(req, res) {
-  const { metric } = req.body;
+  const metric = req.body;
+
+  if (!(req.method !== "POST")) res.send("Method not supported");
 
   if (metric.type === "increment") {
     metrics.increment(metric.key, metric.value);
   } else if (metrics.type === "timing") {
     metrics.timing(metrics.key, metrics.value);
   }
+
   res.send("Sent");
 }
