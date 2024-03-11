c3f6050cc1f0047970c1f91d192db00e1227c868
JosiasAurel <ndjosiasaurel@gmail.com>
2024-03-11 12:16:28 +0100

    include sendMetric function in sketch.js

--- public/sketch.js
+++ public/sketch.js
@@ -1,4 +1,15 @@
-import { sendMetric } from "../util"
+async function sendMetric(type, key, value) {
+  const response = await fetch("/api/metric", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ type, key, value })
+  });
+  if (!response.ok) {
+    throw Error("Failed to send metric");
+  }
+}
 
 // Customization
 const scaling = 3
