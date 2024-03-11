4933e3baa12c3a3c2e8856c63ffee96a08e90953
JosiasAurel <ndjosiasaurel@gmail.com>
2024-03-11 14:01:40 +0100

    add start command

--- draw-dino-auth/server.js
+++ draw-dino-auth/server.js
@@ -43,4 +43,5 @@ app.get(
   }
 )
 
-app.listen(3000, server => console.log('server is running on port', 3000))
+const port = process.env.PORT || 3000
+app.listen(port, server => console.log('server is running on port', port))
